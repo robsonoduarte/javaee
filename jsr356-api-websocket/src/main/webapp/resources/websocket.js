8a7124267e7c924947ce49e83e50e82402ffc312
robsonoduarte <robson.o.d@gmail.com>
2014-03-24 14:47:46 -0300

    add a simple example (draft) of ajax polling for comparation with websocket.

--- jsr356-api-websocket/src/main/webapp/resources/websocket.js
+++ jsr356-api-websocket/src/main/webapp/resources/websocket.js
@@ -1,12 +1,13 @@
 /**
- * 
+ *
  */
 
 
-var connection = new WebSocket('ws://localhost:8080/jsr356-api-websocket/server');
+
+var connection = new WebSocket('ws://10.96.237.180:8080/jsr356-api-websocket/server');
 
 
 connection.onmessage = function(e) {
 		$("#test").append(e.data + "<br>");
-};	
+};
 
